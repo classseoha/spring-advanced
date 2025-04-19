704926a42e5adbfafc4ee9fa4ea4bc428db34fd9
classseoha <classseoha>
2025-04-19 10:20:15 +0900

    레벨 1-3: Validation(UserChangePasswordRequest 암호 검증 메서드  부분)

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -16,6 +16,7 @@ public class UserChangePasswordRequest {
     @NotBlank
     private String newPassword;
 
+    // 비밀번호 검증 코드 추가-
     @AssertTrue(message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.")
     public boolean isValidNewPassword() {
         return newPassword != null && // 새비밀번호가 null인지 확인
